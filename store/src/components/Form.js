12dcf8faae63d092a8a0c17ce76d2b0323e8e5c8
Ankush <ankushtsalian@gmail.com>
2022-12-08 19:59:19 +0530

    pagination

--- store/src/components/Form.js
+++ store/src/components/Form.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "../Styles/form.css";
 import Result from "./Result";
 const URL = "http://localhost:5000/api/v1/products";
-const companyList = ["ikea", "liddy", "caressa", "marcos"];
+const companyList = ["", "ikea", "liddy", "caressa", "marcos"];
 
 const Form = () => {
   const [form, setForm] = useState({
@@ -20,20 +20,28 @@ const Form = () => {
   const { featured, company, name, nameSort, priceSort, page } = form;
 
   const sort = nameSort + "," + priceSort;
-  const pageNumber = pageResponse?.length / 10;
-  console.log(Math.ceil(pageNumber));
+  const pageNumber = Math.ceil(pageResponse.length / 10);
+  //   (response?.length != 0 ? response : pageResponse)?.length / 10
+  // );
 
-  useEffect(
-    () => async () => {
-      try {
-        const res = await axios.get(URL);
-        setPageRes(res.data.msg);
-      } catch (error) {
-        console.log(error.response);
-      }
-    },
-    []
-  );
+  const pageURL = `${URL}?featured=${featured}&company=${company}&name=${name}`;
+
+  const pagefn = async () => {
+    try {
+      const res = await axios.get(pageURL);
+      setPageRes(() => res.data.msg);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  useEffect(() => {
+    pagefn();
+    return () => {
+      console.log("DONE");
+    };
+  }, [response]);
+
   const handleCheckbox = (event) => {
     setForm((formValues) => ({ ...formValues, featured: !featured }));
   };
@@ -50,119 +58,130 @@ const Form = () => {
 
   const handlePage = (e, i) => {
     e.preventDefault();
-    setForm((formValues) => ({ ...formValues, page: i }));
+    setForm((formValues) => ({ ...formValues, page: i + 1 }));
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
       const res = await axios.get(
-        `${URL}?featured=${featured}&company=${company}&name=${name}&sort=${sort}&page=${page}`
+        `${URL}?featured=${featured}&company=${company}&name=${name}&sort=${sort}&page=${
+          page > 0 ? page : ""
+        }`
       );
-      setResponse(res.data.msg);
+      console.log(res.data.msg.length);
+      setResponse(() => res.data.msg);
     } catch (error) {
       console.log(error.response);
     }
   };
+
+  // console.log("pageResponse : " + pageResponse.length);
+  // console.log("pageNumber : " + Math.ceil(pageNumber));
+  // console.log(featured, company, name);
+  console.log(response);
   return (
     <div>
       <p>{nameSort}</p>
       <p>{priceSort}</p>
-      <form className="form">
-        {/* ----------------------------INPUTS---------------------------------------- */}
-        <div className="formContainer">
-          <div className="form-row-container">
-            <label>Company : </label>
-            <div className="form-row">
-              <select onChange={handleSelect}>
-                {companyList.map((opt) => (
-                  <option key={opt} value={opt}>
-                    {opt}
-                  </option>
-                ))}
-              </select>
+      <p>{page}</p>
+      <form className="form-container">
+        <div className="form">
+          {/* ----------------------------INPUTS---------------------------------------- */}
+          <div className="formContainer">
+            <div className="form-row-container">
+              <label>Company : </label>
+              <div className="form-row">
+                <select onChange={handleSelect}>
+                  {companyList.map((opt) => (
+                    <option key={opt} value={opt}>
+                      {opt}
+                    </option>
+                  ))}
+                </select>
+              </div>
             </div>
-          </div>
-          <div className="form-row-container">
-            <label>name : </label>
-            <div className="form-row">
-              <input onChange={handleName} />
+            <div className="form-row-container">
+              <label>name : </label>
+              <div className="form-row">
+                <input onChange={handleName} />
+              </div>
             </div>
-          </div>
-          <div className="form-row-container">
-            <label>Featured : </label>
-            <div className="form-row">
-              <input
-                type="checkbox"
-                value={featured}
-                onChange={handleCheckbox}
-              />
+            <div className="form-row-container">
+              <label>Featured : </label>
+              <div className="form-row">
+                <input
+                  type="checkbox"
+                  value={featured}
+                  onChange={handleCheckbox}
+                />
+              </div>
             </div>
+            <button onClick={handleSubmit}>Submit</button>
           </div>
-          <button onClick={handleSubmit}>Submit</button>
-        </div>
-        {/* ----------------------------SORTS---------------------------------------- */}
-        <div className="formContainer">
-          <div className="form-row-container">
-            <label>Sort by name : </label>
-            <div className="form-row">
-              <button
-                onClick={(e) => {
-                  e.preventDefault();
-                  return setForm((formValues) => ({
-                    ...formValues,
-                    nameSort: "name",
-                  }));
-                }}
-              >
-                A to Z
-              </button>
-              <button
-                onClick={(e) => {
-                  e.preventDefault();
-                  return setForm((formValues) => ({
-                    ...formValues,
-                    nameSort: "-name",
-                  }));
-                }}
-              >
-                Z to A
-              </button>
+          {/* ----------------------------SORTS---------------------------------------- */}
+          <div className="formContainer">
+            <div className="form-row-container">
+              <label>Sort by name : </label>
+              <div className="form-row">
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    return setForm((formValues) => ({
+                      ...formValues,
+                      nameSort: "name",
+                    }));
+                  }}
+                >
+                  A to Z
+                </button>
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    return setForm((formValues) => ({
+                      ...formValues,
+                      nameSort: "-name",
+                    }));
+                  }}
+                >
+                  Z to A
+                </button>
+              </div>
             </div>
-          </div>
-          <div className="form-row-container">
-            <label>Sort by Price</label>
-            <div className="form-row">
-              <button
-                onClick={(e) => {
-                  e.preventDefault();
-                  return setForm((formValues) => ({
-                    ...formValues,
-                    priceSort: "-price",
-                  }));
-                }}
-              >
-                High to Low
-              </button>
-              <button
-                onClick={(e) => {
-                  e.preventDefault();
-                  return setForm((formValues) => ({
-                    ...formValues,
-                    priceSort: "price",
-                  }));
-                }}
-              >
-                Low to High
-              </button>
+            <div className="form-row-container">
+              <label>Sort by Price</label>
+              <div className="form-row">
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    return setForm((formValues) => ({
+                      ...formValues,
+                      priceSort: "-price",
+                    }));
+                  }}
+                >
+                  High to Low
+                </button>
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    return setForm((formValues) => ({
+                      ...formValues,
+                      priceSort: "price",
+                    }));
+                  }}
+                >
+                  Low to High
+                </button>
+              </div>
             </div>
           </div>
         </div>
         {/* ---------------------------PAGINATION---------------------------------------- */}
         <div className="pagination-container">
-          {response.map((a, i) => (
+          {Array.from(Array(pageNumber), (_, i) => (
             <button key={i} onClick={(e) => handlePage(e, i)}>
-              {i}
+              {i + 1}
             </button>
           ))}
         </div>
